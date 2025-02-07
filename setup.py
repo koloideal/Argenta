93438f8e4550ec165e859600068fe8e762bf9b7b
kolo <kolo.is.main@gmail.com>
2025-02-07 18:35:08 +0300

    v0.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="argenta",
-    version="0.1.1",
+    version="0.1.2",
     author="kolo",
     author_email="kolo.is.main@gmail.com",
     description="Python library for creating CLI apps",
