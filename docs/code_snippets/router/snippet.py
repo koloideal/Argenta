1eaf2b6333068040f2a349a2b36e9ca10f9253d8
kolo <kolo.is.main@gmail.com>
2025-11-27 21:50:30 +0300

    update

--- docs/code_snippets/router/snippet.py
+++ docs/code_snippets/router/snippet.py
@@ -1,10 +1,7 @@
-from argenta.command import Command
-from argenta.router import Router
+from argenta import Command, Response, Router
 
 user_router = Router(title="User Management")
 
-
 @user_router.command(Command("add-user", description="Adds a new user"))
-def add_user_handler(response):
-    # Логика добавления пользователя
+def add_user_handler(response: Response):
     print("User added successfully!")
