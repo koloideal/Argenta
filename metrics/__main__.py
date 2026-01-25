0567a3f4a366b48a5853d0d59f61f2b5abfdf23c
kolo <kolo.is.main@gmail.com>
2026-01-25 16:04:47 +0300

    release generate

--- metrics/__main__.py
+++ metrics/__main__.py
@@ -8,7 +8,9 @@ orchestrator = Orchestrator()
 
 def main() -> None:
     app.include_router(router)
-    app.set_description_message_pattern(lambda command, description: f'[blue]{command}[/blue] [red]--->[/red] [i]{description}[/i]')
+    app.set_description_message_pattern(
+        lambda command, description: f'[bold cyan]▸[/bold cyan] [bold white]{command}[/bold white] [dim]│[/dim] [yellow italic]{description}[/yellow italic]'
+    )
     orchestrator.start_polling(app)
 
 
