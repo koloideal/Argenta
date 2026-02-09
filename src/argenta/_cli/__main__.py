18a8376469190b0015f66ac51aa8ca7ff0dde72b
kolo <kolo.is.main@gmail.com>
2026-02-09 14:50:03 +0300

    add new init command for generate boilerplate in flat or src layout

--- src/argenta/_cli/__main__.py
+++ src/argenta/_cli/__main__.py
@@ -1,12 +1,12 @@
 from typer import Typer
 
-from .commands import run_handler
+from .commands import run_handler, init_handler
 
 
 def main():
     app = Typer()
     app.command("run")(run_handler)
-    app.command("init")(run_handler)
+    app.command("init")(init_handler)
     app()
 
 if __name__ == '__main__':
--- src/argenta/_cli/commands/__init__.py
+++ src/argenta/_cli/commands/__init__.py
@@ -1,1 +1,2 @@
-from .run import run_handler
+from .run import run_handler as run_handler
+from .init import init_handler as init_handler
